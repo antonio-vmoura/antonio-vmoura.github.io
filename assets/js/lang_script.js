7045f666dbdf4d8c982c43ec295ed7fd575026eb
Antônio Rodrigues <avmoura.r@gmail.com>
2024-10-05 12:46:58 -0300

    :bento: add: new assets

--- assets/js/lang_script.js
+++ assets/js/lang_script.js
@@ -1,4 +1,6 @@
 var language_button = document.getElementById("lang_button");
+var resume_button = document.getElementById("resume_file");
+
 var en = document.getElementsByClassName("lang_en");
 var pt = document.getElementsByClassName("lang_pt");
 
@@ -6,16 +8,19 @@ var lang_en = true;
 
 function changeLanguage() {
   if (lang_en == false) {
-    language_button.innerHTML = "Website language: &#x1F1FA;&#x1F1F8;";
     lang_en = true;
+    language_button.innerHTML = "Website language: &#x1F1FA;&#x1F1F8;";
+    resume_button.href="https://antonio-vmoura.github.io/assets/docs/antonio_vinicius_resume_en.pdf";
 
     for (i = 0; i < en.length; i++) {
       en[i].style.display = "block";
       pt[i].style.display = "none";
     }
   } else {
-    language_button.innerHTML = "Idioma do site: &#x1F1E7;&#x1F1F7;";
     lang_en = false;
+    language_button.innerHTML = "Idioma do site: &#x1F1E7;&#x1F1F7;";
+    resume_button.href="https://antonio-vmoura.github.io/assets/docs/antonio_vinicius_resume_pt.pdf";
+    
 
     for (i = 0; i < en.length; i++) {
       en[i].style.display = "none";
